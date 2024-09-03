cc771cec67ee2dec8c149b181a5b78ea383c4883
Gunski <gunnarkryth@gmail.com>
2024-09-03 12:03:26 +0200

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,83 @@ export const Home = () => {
 
   return (
     <div>
-      <h1>Home page</h1>
+      <section>
+        <article>
+          <h1>Home page</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum fuga
+            nostrum temporibus voluptate itaque quisquam minus, minima dolorum
+            similique dolorem qui alias, quod voluptates ut eligendi, explicabo
+            enim. Adipisci, natus?
+          </p>
+        </article>
+        <article>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum fuga
+            nostrum temporibus voluptate itaque quisquam minus, minima dolorum
+            similique dolorem qui alias, quod voluptates ut eligendi, explicabo
+            enim. Adipisci, natus?
+          </p>
+        </article>
+        <article>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum fuga
+            nostrum temporibus voluptate itaque quisquam minus, minima dolorum
+            similique dolorem qui alias, quod voluptates ut eligendi, explicabo
+            enim. Adipisci, natus?
+          </p>
+        </article>
+        <article>
+          <h1>Home page</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum fuga
+            nostrum temporibus voluptate itaque quisquam minus, minima dolorum
+            similique dolorem qui alias, quod voluptates ut eligendi, explicabo
+            enim. Adipisci, natus?
+          </p>
+        </article>
+        <article>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum fuga
+            nostrum temporibus voluptate itaque quisquam minus, minima dolorum
+            similique dolorem qui alias, quod voluptates ut eligendi, explicabo
+            enim. Adipisci, natus?
+          </p>
+        </article>
+        <article>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum fuga
+            nostrum temporibus voluptate itaque quisquam minus, minima dolorum
+            similique dolorem qui alias, quod voluptates ut eligendi, explicabo
+            enim. Adipisci, natus?
+          </p>
+        </article>
+        <article>
+          <h1>Home page</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum fuga
+            nostrum temporibus voluptate itaque quisquam minus, minima dolorum
+            similique dolorem qui alias, quod voluptates ut eligendi, explicabo
+            enim. Adipisci, natus?
+          </p>
+        </article>
+        <article>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum fuga
+            nostrum temporibus voluptate itaque quisquam minus, minima dolorum
+            similique dolorem qui alias, quod voluptates ut eligendi, explicabo
+            enim. Adipisci, natus?
+          </p>
+        </article>
+        <article>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum fuga
+            nostrum temporibus voluptate itaque quisquam minus, minima dolorum
+            similique dolorem qui alias, quod voluptates ut eligendi, explicabo
+            enim. Adipisci, natus?
+          </p>
+        </article>
+      </section>
     </div>
   );
 };
